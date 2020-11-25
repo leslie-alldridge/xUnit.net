25afda1b03d3a011e86cefcfe1e41c70fb47b059
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-26 08:52:02 +1300

    Add multiple setup methods

--- CreditCard/CreditCardUnitTests/CreditCardApplicationUnitTests.cs
+++ CreditCard/CreditCardUnitTests/CreditCardApplicationUnitTests.cs
@@ -39,8 +39,28 @@ namespace CreditCardUnitTests
         {
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
 
+            // returns true when the specific string "x" is passed to isValid method
             mockValidator.Setup(x => x.IsValid("x")).Returns(true);
 
+            // returns true when any string is passed to the isValid method
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
+            
+            // returns true when any string is passed to the isValid method that begins with "x"
+            mockValidator.Setup(x => 
+                    x.IsValid(It.Is<string>(x => x.StartsWith("x"))))
+                .Returns(true);
+
+            // returns true when any string is passed to the isValid method that is between a-z
+            mockValidator.Setup(x =>
+                x.IsValid(It.IsInRange("a", "z", Range.Inclusive))).Returns(true);
+
+            // returns true when the string is either "x" or "z"
+            mockValidator.Setup(x =>
+                x.IsValid(It.IsIn("x", "z"))).Returns(true);
+
+            // return true when a regex is satisfied
+            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z"))).Returns(true);
+            
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
             var application = new CreditCardApplication
